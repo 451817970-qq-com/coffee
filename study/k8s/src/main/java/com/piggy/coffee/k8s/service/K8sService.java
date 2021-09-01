76a2b6649753ea567b48f6297f64a93e91e54299
451817970@qq.com <451817970@qq.com>
2021-09-01 14:49:23 +0800

    k8s env

--- study/k8s/src/main/java/com/piggy/coffee/k8s/service/K8sService.java
+++ study/k8s/src/main/java/com/piggy/coffee/k8s/service/K8sService.java
@@ -26,78 +26,83 @@ public class K8sService {
 	@Autowired
 	private K8sClientConfig k8sClientConfig;
 
-	 public void createPod(CoffeePod cPod) {
+	public void createPod(CoffeePod cPod) {
 
-	        KubernetesClient client = K8sClientUtils.getClient(k8sClientConfig);
+		KubernetesClient client = K8sClientUtils.getClient(k8sClientConfig);
 
-	        // metadata
-	        PodBuilder pb = new PodBuilder().withNewMetadata().withName(cPod.getName()).addToAnnotations("seccomp.security.alpha.kubernetes.io/pod", "unconfined").endMetadata();
+		// metadata
+		PodBuilder pb = new PodBuilder().withNewMetadata().withName(cPod.getName())
+				.addToAnnotations("seccomp.security.alpha.kubernetes.io/pod", "unconfined").endMetadata();
 
-	        // container
-	        SpecNested<PodBuilder> specNested = pb.withNewSpec().addNewContainer().withName(K8sUtils.buildContainerName(cPod.getContainerName()))
-	                .withImage(cPod.getImage()).withResources(null).addNewVolumeMount().withName(K8sCsts.CONFIG_DIR)
-	                .withMountPath(K8sCsts.CONFIG_DIR_CONTAINER_PATH).endVolumeMount().addNewVolumeMount().withName(K8sCsts.SEED_DIR)
-	                .withMountPath(cPod.getSeedDirContainerPath()).endVolumeMount()
-	                .addNewCommand("bash")
-	                .addNewCommand("-c")
-	                .addNewCommand("while true; do echo \"hello aaa\"; sleep 3; done")
-	                .withNewSecurityContext().withPrivileged(true).editOrNewCapabilities().addToAdd("SYS_PTRACE").endCapabilities().endSecurityContext()
-	                .endContainer();
+		// container
+		SpecNested<PodBuilder> specNested = pb.withNewSpec().addNewContainer()
+				.withName(K8sUtils.buildContainerName(cPod.getContainerName())).withImage(cPod.getImage())
+				.withResources(null)
+				//
+				.addNewEnv().withName("POD_IP").editOrNewValueFrom().editOrNewFieldRef().withFieldPath("status.podIP").endFieldRef().endValueFrom().endEnv()
+				//
+				.addNewEnv().withName("MASTER_IP").withValue("xx.xx.xx.xx").endEnv()
+				//
+				.addNewVolumeMount().withName(K8sCsts.CONFIG_DIR).withMountPath(K8sCsts.CONFIG_DIR_CONTAINER_PATH)
+				.endVolumeMount()
+				//
+				.addNewVolumeMount().withName(K8sCsts.SEED_DIR).withMountPath(cPod.getSeedDirContainerPath())
+				.endVolumeMount()
+				//
+				.addNewCommand("bash").addNewCommand("-c")
+				.addNewCommand("while true; do echo \"hello aaa\"; sleep 3; done").withNewSecurityContext()
+				.withPrivileged(true).editOrNewCapabilities().addToAdd("SYS_PTRACE").endCapabilities()
+				.endSecurityContext().endContainer();
 
-	        // volume
-	        specNested.addNewVolume().withName(K8sCsts.CONFIG_DIR).withNewHostPath(cPod.getConfigDirHostPath(), null)
-	                .endVolume();
-	        specNested.addNewVolume().withName(K8sCsts.SEED_DIR).withNewHostPath(cPod.getSeedDirHostPath(), null)
-	                .endVolume();
-	        
-	        Pod pod = specNested.withNewRestartPolicy("Always").endSpec().build();
+		// volume
+		specNested.addNewVolume().withName(K8sCsts.CONFIG_DIR).withNewHostPath(cPod.getConfigDirHostPath(), null)
+				.endVolume();
+		specNested.addNewVolume().withName(K8sCsts.SEED_DIR).withNewHostPath(cPod.getSeedDirHostPath(), null)
+				.endVolume();
 
-	        client.pods().inNamespace(cPod.getNamespace()).create(pod);
+		Pod pod = specNested.withNewRestartPolicy("Always").endSpec().build();
 
-	    }
+		client.pods().inNamespace(cPod.getNamespace()).create(pod);
 
-	    
-	    public Pod getPod(CoffeePod cPod) {
-	        KubernetesClient client = K8sClientUtils.getClient(k8sClientConfig);
-	        return client.pods().inNamespace(cPod.getNamespace()).withName(cPod.getName()).get();
-	    }
+	}
 
-	    
-	    public void detelePod(CoffeePod cPod) {
-	        KubernetesClient client = K8sClientUtils.getClient(k8sClientConfig);
-	        client.pods().inNamespace(cPod.getNamespace()).withName(cPod.getName()).delete();
-	    }
+	public Pod getPod(CoffeePod cPod) {
+		KubernetesClient client = K8sClientUtils.getClient(k8sClientConfig);
+		return client.pods().inNamespace(cPod.getNamespace()).withName(cPod.getName()).get();
+	}
 
-	    
-	    public void detelePodSync(CoffeePod cPod) {
-	        String cmd = "kubectl --kubeconfig=" + k8sClientConfig.getKubeConfig() + " delete pods " + cPod.getName()
-	                + " --now=true --wait=true";
-	        ShellExeResult seResult = ShellUtils.executeAndGetExitStatus(cmd);
+	public void detelePod(CoffeePod cPod) {
+		KubernetesClient client = K8sClientUtils.getClient(k8sClientConfig);
+		client.pods().inNamespace(cPod.getNamespace()).withName(cPod.getName()).delete();
+	}
+
+	public void detelePodSync(CoffeePod cPod) {
+		String cmd = "kubectl --kubeconfig=" + k8sClientConfig.getKubeConfig() + " delete pods " + cPod.getName()
+				+ " --now=true --wait=true";
+		ShellExeResult seResult = ShellUtils.executeAndGetExitStatus(cmd);
 //	        if (seResult.getExitCode() != 0) {
 //	            throw new RuntimeException(seResult.getOut());
 //	        }
 
-	    }
+	}
 
-	    
-	    public ShellExeResult exec(CoffeePod cPod, CoffeeCmd cCmd) {
-	        String cmd = K8sCmdUtils.buildCmd(cCmd, cPod, k8sClientConfig);
-	        return ShellUtils.executeAndGetExitStatus(cmd);
+	public ShellExeResult exec(CoffeePod cPod, CoffeeCmd cCmd) {
+		String cmd = K8sCmdUtils.buildCmd(cCmd, cPod, k8sClientConfig);
+		return ShellUtils.executeAndGetExitStatus(cmd);
 
-	    }
+	}
 
-	    
-	    public NodeList getNode() {
-	        KubernetesClient client = K8sClientUtils.getClient(k8sClientConfig);
-	        return client.nodes().list();
-	    }
+	public NodeList getNode() {
+		KubernetesClient client = K8sClientUtils.getClient(k8sClientConfig);
+		return client.nodes().list();
+	}
 
-	    // metrics start ...
-	    
-	    public NodeMetricsList getNodeMetrics() {
-	        MetricAPIGroupClient mClient = K8sClientUtils.getMetricsClient(k8sClientConfig);
-	        NodeMetricsList nmList = mClient.nodes().metrics();
-	        return nmList;
-	    }
+	// metrics start ...
+
+	public NodeMetricsList getNodeMetrics() {
+		MetricAPIGroupClient mClient = K8sClientUtils.getMetricsClient(k8sClientConfig);
+		NodeMetricsList nmList = mClient.nodes().metrics();
+		return nmList;
+	}
 
 }
