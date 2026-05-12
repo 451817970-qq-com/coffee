df7237ef9c0072d03d0f8b1bf47b27aeb8f7c21e
mumu <451817970@qq.com>
2026-05-12 11:18:20 +0800

    a

--- common-jars/common/src/test/java/com/piggy/coffee/common/util/net/IpUtilsTest.java
+++ common-jars/common/src/test/java/com/piggy/coffee/common/util/net/IpUtilsTest.java
@@ -3,7 +3,6 @@ package com.piggy.coffee.common.util.net;
 import org.junit.Test;
 
 public class IpUtilsTest {
-	
 
 	@Test
 	public void test() {
--- common-jars/common/src/test/java/com/piggy/coffee/tmp/TmpTest.java
+++ common-jars/common/src/test/java/com/piggy/coffee/tmp/TmpTest.java
@@ -1,53 +1,13 @@
 package com.piggy.coffee.tmp;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 public class TmpTest {
-
-	@Test
-	public void test() {
-		int COUNT_BITS = Integer.SIZE - 3;
-		System.out.println(COUNT_BITS);
-		int STOP = (-1 << COUNT_BITS);
-		
-		System.out.println(Integer.toBinaryString(STOP));
-		System.out.println(STOP);
-		
-		AtomicInteger ctl = new AtomicInteger(ctlOf(STOP, 0));
-		System.out.println(Integer.toBinaryString(ctl.get()));
-		System.out.println(ctl.get());
-		
-//		int CAPACITY = (1 << COUNT_BITS) - 1;
-//		System.out.println(Integer.toBinaryString(CAPACITY));
-	}
-	
-    private static int ctlOf(int rs, int wc) { return rs | wc; }
-
-	@Test
-	public void test2() {
-
-		// math.abs(6*math.pow(2.71828183,expectElem)-113*expectElem+17)<=0.00001
-		double d = Math.abs(6 * Math.pow(2.71828183, 0.216366) - 113 * 0.216366 + 17);
-		double d2 = Math.abs(6 * Math.pow(2.71828183, 4.377021) - 113 * 4.377021 + 17);
-		System.out.println(d);
-		System.out.println(d2);
-
-		System.out.println(Math.abs(d) < 0.00001);
-		System.out.println(Math.abs(d2) < 0.00001);
-	}
-
-	@Test
-	public void test3() {
-
-		// math.abs(6*math.pow(2.71828183,expectElem)-113*expectElem+17)<=0.00001
-		double d = Math.abs(6 * Math.pow(2.718281828459045, 0.216366) - 113 * 0.216366 + 17);
-		double d2 = Math.abs(6 * Math.pow(2.718281828459045, 4.377021) - 113 * 4.377021 + 17);
-		System.out.println(d);
-		System.out.println(d2);
-	}
-
 	@Test
 	public void testHouse() {
 		int years = 50;
@@ -55,6 +15,18 @@ public class TmpTest {
 		double perMonth = perYear / 12;
 		System.out.println("每年花费：" + perYear);
 		System.out.println("每月花费：" + perMonth);
+	}
+
+	@Test
+	public void testLambda() {
+		List<Integer> list = Arrays.asList(1, 2, 3);
+		Integer val = list.stream()
+				.map(i -> i * 2)
+				.map(i -> i.toString())
+				.map(i -> Integer.valueOf(i) * 2).findFirst()
+				.get();
+		assertEquals(4, val);
+							
 	}
 
 }
